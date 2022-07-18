8f910c346be1a056ad30b0d7eac4183ff8ef4e48
Salman Mujtaba <salmanmujtaba@macbook.local>
2022-07-18 02:54:34 -0400

    Major changes related to the project

--- script.js
+++ script.js
@@ -3,28 +3,39 @@ document.addEventListener("DOMContentLoaded", (e) => {
   form.addEventListener("submit", (e) => {
     validateForm(e);
   });
+  document.getAnimations()
 });
 
 function focusElement(e) {
-
+  e.target.parentElement.classList.remove("invalid");
   e.target.nextElementSibling.classList.remove('hide');
   e.target.previousElementSibling.classList.add('label-animate');
 
 }
 
 function blurElement(e) {
+  if (e.target.value.trim() === "") {
+    e.target.previousElementSibling.classList.remove('label-animate');
+  }
+  if (e.target.checkValidity()) {
+    e.target.classList.remove("invalid");
+    e.target.setAttribute("aria-invalid", "false");
+  } else {
+    e.target.setAttribute("aria-invalid", "true");
+    e.target.parentElement.classList.add("invalid");
+
+  }
   e.target.nextElementSibling.classList.add('hide');
 }
 
 function remove(event) {
-  console.log(event, "===");
+  console.log(event);
   // event.target.previousElementSibling.value = "";
   // event.target.classList.remove('label-animate');
 }
 
 function validateForm(e) {
   const form = e.target;
-  // console.log(form.checkValidity());
   if (form.checkValidity()) {
     // form is valid - make further checks
   } else {
@@ -32,7 +43,6 @@ function validateForm(e) {
     e.preventDefault();
     // apply invalid class
     Array.from(form.elements).forEach(i => {
-      console.log(i, i.checkValidity());
       if (i.type === "text" || i.type === "email") {
         if (i.checkValidity()) {
           i.parentElement.classList.remove('invalid');
