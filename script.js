26bb7899191a2638be1b12b3f65b9397e98c5bc1
Salman Mujtaba <salmanmujtaba@macbook.local>
2022-09-15 00:20:22 -0400

    Simple Lyft carousel

--- script.js
+++ script.js
@@ -1,61 +1,32 @@
+let cardContainer;
+let currentShift = 0;
+let offset = 288;
+let maxShift = 1625;
+let minShift = 0;
 document.addEventListener("DOMContentLoaded", (e) => {
-  let form = document.getElementById('form');
-  form.addEventListener("submit", (e) => {
-    validateForm(e);
-  });
-  document.getAnimations()
+  let btn = document.querySelector("#button-container");
+  btn.addEventListener("click", buttonClick);
+  cardContainer = document.querySelector('.card-container')
 });
 
-function focusElement(e) {
-  e.target.parentElement.classList.remove("invalid");
-  e.target.nextElementSibling.classList.remove('hide');
-  e.target.previousElementSibling.classList.add('label-animate');
+function buttonClick(e) {
+  console.log(e);
 
 }
 
-function blurElement(e) {
-  if (e.target.value.trim().length === 0) {
-    e.target.previousElementSibling.classList.remove('label-animate');
-    e.target.nextElementSibling.classList.add('hide');
-  }
-  if (e.target.checkValidity()) {
-    e.target.classList.remove("invalid");
-    e.target.setAttribute("aria-invalid", "false");
-  } else {
-    e.target.setAttribute("aria-invalid", "true");
-    e.target.parentElement.classList.add("invalid");
-
+function next() {
+  if (Math.abs(currentShift) < Math.abs(maxShift)) {
+    currentShift = currentShift - offset;
+    cardContainer.style.transform = `translateX(${currentShift}px)`;
+    window.requestAnimationFrame(() => {});
   }
 }
 
-function remove(event) {
-  let input = event.target.parentElement.previousElementSibling
-  input.value = "";
-  input.previousElementSibling.classList.remove('label-animate');
-  input.nextElementSibling.classList.add('hide');
-  input.setAttribute("aria-invalid", "true");
-  input.parentElement.classList.add("invalid");
+function prev() {
+  if (Math.abs(currentShift) > Math.abs(minShift)) {
+    currentShift = currentShift + offset;
+    cardContainer.style.transform = `translateX(${currentShift}px)`;
+    window.requestAnimationFrame(() => {});
 
-}
-
-function validateForm(e) {
-  const form = e.target;
-  if (form.checkValidity()) {
-    // form is valid - make further checks
-  } else {
-    // form is invalid - cancel submit
-    e.preventDefault();
-    // apply invalid class
-    Array.from(form.elements).forEach(i => {
-      if (i.type === "text" || i.type === "email") {
-        if (i.checkValidity()) {
-          i.parentElement.classList.remove('invalid');
-        } else {
-
-          // field is invalid - add class
-          i.parentElement.classList.add('invalid');
-        }
-      }
-    });
   }
-};
+}
